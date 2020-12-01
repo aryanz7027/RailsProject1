49e3f7ee9cd4236ca27b11d3d7eb24634fb4be4b
aryanz7027 <aryanz7027@gmail.com>
2020-12-01 14:26:09 +0530

    Create articles and saving in DB frontend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
     def show
-        @article =Article.find(1)
+        @article =Article.find(params[:id])
+    end
+    def index
+        @articles=Article.all
+    end
+    def new
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to  @article
     end
 end
