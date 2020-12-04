7805d0e6bf73a1e5bbaa45992b94472557536468
aryanz7027 <aryanz7027@gmail.com>
2020-12-04 12:39:33 +0530

    Add one-to-many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    vlaidates :username, presence: true, 
-                    uniqueness: { case_sensitive: false }, length {minimum:3 , maximum: 25}
+    has_many :articles
+    validates :username, presence: true, 
+                    uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     VALID_EAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    vlaidates :email, presence: true,
-                    uniqueness: { case_sensitive: false }, length {maximum: 105}, format: {with: VALID_EAIL_REGEX}
+    validates :email, presence: true,
+                    uniqueness: { case_sensitive: false }, length: {maximum: 105}, format: {with: VALID_EAIL_REGEX}
 end
