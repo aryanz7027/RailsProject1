21a97c35fd5fd86e743884680f69683cff92fb66
aryanz7027 <aryanz7027@gmail.com>
2020-12-04 11:49:44 +0530

    user/email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    vlaidates :username, presence: true, 
+                    uniqueness: { case_sensitive: false }, length {minimum:3 , maximum: 25}
+    VALID_EAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    vlaidates :email, presence: true,
+                    uniqueness: { case_sensitive: false }, length {maximum: 105}, format: {with: VALID_EAIL_REGEX}
 end
