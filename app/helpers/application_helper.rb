19e8879b64a6bda939c797500c9173dffe5e923f
aryanz7027 <aryanz7027@gmail.com>
2020-12-06 20:17:17 +0530

    Show user page created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def gravtar_for(user, options = { size: 80})
+        email_address = user.email.downcase
+        size = options[:size]
+        hash = Digest::MD5.hexdigest(email_address)
+        image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(image_src,alt: user.username)
+    end
+
 end
